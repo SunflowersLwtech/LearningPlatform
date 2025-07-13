121624655aaea526b0f90f8be4b8d4f796b5c7ad
SunflowersLwtech <TP085412@mail.apu.edu.my>
2025-07-13 17:53:22 +0800

    🔧 修复MongoDB连接配置 - 解决Render部署问题

--- config/database.js
+++ config/database.js
@@ -9,12 +9,9 @@ const connectDB = async () => {
     }
 
     const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       maxPoolSize: 10, // Maximum number of connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
       bufferCommands: false, // Disable mongoose buffering
     };
 
